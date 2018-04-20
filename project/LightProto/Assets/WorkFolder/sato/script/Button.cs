a4672491bbb9de21d465f70aca355c2eeb641b0b
sato <telrinrin02486@gmail.com>
2018-04-20 12:14:23 +0900

    buttonの追加実装

---
+++ project/LightProto/Assets/WorkFolder/sato/script/Button.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    /// ボタンをクリックした時の処理
+    public void OnClick()
+    {
+        switch (transform.name)
+        {
+            case "CAM1A":
+                Debug.Log("1A");
+                break;
+            case "CAM1B":
+                Debug.Log("1B");
+                break;
+            case "CAM1C":
+                Debug.Log("1C");
+                break;
+            case "CAM2A":
+                Debug.Log("2A");
+                break;
+            case "CAM2B":
+                Debug.Log("2B");
+                break;
+            case "CAM4A":
+                Debug.Log("4A");
+                break;
+            case "CAM4B":
+                Debug.Log("4B");
+                break;
+            case "CAM3":
+                Debug.Log("3");
+                break;
+            case "CAM5":
+                Debug.Log("5");
+                break;
+            case "CAM6":
+                Debug.Log("6");
+                break;
+            case "CAM7":
+                Debug.Log("7");
+                break;
+            default:
+                break;
+        }
+    }
+}
